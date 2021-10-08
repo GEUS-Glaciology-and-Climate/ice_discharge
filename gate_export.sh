50c6e3d450eba138bed5409ca077868cdd5f7a56
Kenneth D. Mankoff <mankoff@gmail.com>
2021-10-08 08:10:57 +0200

    Gate GeoJSON

--- gate_export.sh
+++ gate_export.sh
@@ -7,4 +7,5 @@ g.mapset gates_100_5000
 v.out.ogr input=gates_final output=./out/gates.kml format=KML --o
 (cd out; zip gates.kmz gates.kml; rm gates.kml)
 v.out.ogr input=gates_final output=./out/gates.gpkg format=GPKG --o
+v.out.ogr input=gates_final output=./out/gates.geojson format=GeoJSON --o
 # Gates:1 ends here
